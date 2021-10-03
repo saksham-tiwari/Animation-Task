4e8c5b622a321798d9d7fed8f69f5beae9fc0bcb
saksham-tiwari <lucky121saty@gmail.com>
2021-10-03 23:17:30 +0530

    Added images, svgs and starting animations

--- script/index.js
+++ script/index.js
@@ -1,15 +1,34 @@
-function myMove() {
-    let id = null;
-    const elem = document.getElementById("animate");   
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 590) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = pos + "px"; 
-      }
+var id = null;
+
+forward();
+function forward() {
+  var elem = document.querySelector(".block");  
+  var elem2 = document.querySelector(".block .bike img");  
+  elem2.classList.remove('reverse');
+  var count = 0;
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  bikeInterval = setInterval(bike , 50);
+  function frame() {
+      pos+=1.25; 
+      elem.style.backgroundPosition = -pos + 'px -50px'; 
+  }
+  function bike(){
+    if(count === 0){
+        elem2.style.bottom = '14px';
+        count++;
+    }
+    else if (count === 1){
+        elem2.style.bottom = '16px';
+        count++;
+    }
+    else if(count === 2){
+        elem2.style.bottom = '14px';
+        count++
+    }
+    else{
+        count = 0;
     }
   }
+}
